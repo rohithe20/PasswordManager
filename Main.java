ab798992753ccb3d304092f356ccb8bdab9bc588
benkurzion <benkurzion@gmail.com>
2024-09-21 11:33:19 -0400

    buggy

--- Main.java
+++ Main.java
@@ -7,11 +7,7 @@ import java.io.*;
 import java.nio.file.*;
 import java.security.SecureRandom;
 import java.security.spec.KeySpec;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.*;
 
 /*
 
@@ -22,41 +18,26 @@ PROJECT: CSDS 344 PASSWORD MANAGER
 
  */
 
-public class PasswordManager {
+public class Main {
+
+    // To be used for encrypting and decrypting user data
+    private static byte[] encryptionKey = new byte[1];
+
     public static void main(String[] args) {
 
-        /**
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
-        random.nextBytes(salt);
-        String base64Salt = Base64.getEncoder().encodeToString(salt);
-        String knownData = "Hello";
-        String passWord = "Desk";
-
-        byte[] h = hashPassword(salt, passWord);
-        String token = encrypt(h, knownData);
-
-        System.out.println(decrypt(h, token));
-         */
-
-
-
-
         // Check if password file exists or not
-
         Scanner scanner = new Scanner(System.in);
         String userEnteredPassword = "";
 
-        // check for a password file
         File file = getPasswordFile();
         boolean noFile = false;
 
         try {
             noFile = file.createNewFile();
-            System.out.println(noFile);
+            //System.out.println(noFile);
         }
         catch (IOException e) {
-           System.out.println("Error with file checking");
+            System.out.println("Error with file checking");
         }
 
         if (!noFile){
@@ -64,9 +45,10 @@ public class PasswordManager {
             System.out.println("Please enter the passcode to access your passwords:");
             userEnteredPassword = scanner.nextLine();
             while (!correctPassword){
-
-                //String userEnteredPassword = scanner.nextLine();
                 if (verifyPassword(userEnteredPassword, file, "hello")){
+                    // this is the correct password and will be saved for later use in this session
+                    String base64Salt = Objects.requireNonNull(getSaltAndToken())[0];
+                    encryptionKey = hashPassword(Base64.getDecoder().decode(base64Salt), userEnteredPassword);
                     correctPassword = true;
                 }else{
                     System.out.println("Wrong password entered");
@@ -81,16 +63,6 @@ public class PasswordManager {
             System.out.println("No password file detected. Creating a new password file.");
             System.out.println("Please enter a passcode to access your passwords:");
             userEnteredPassword = scanner.nextLine();
-            /**
-            try{
-                if (file.createNewFile()){
-                    System.out.println("No password file detected. Creating a new password file.");
-                }
-            }catch (Exception e){
-                System.out.println("Error in creating a file");
-            }
-             */
-
             // make the salt
 
             SecureRandom random = new SecureRandom();
@@ -98,13 +70,16 @@ public class PasswordManager {
             random.nextBytes(salt);
             String base64Salt = Base64.getEncoder().encodeToString(salt);
 
-            // DO SOMETHING WITH THE PASSWORD. HASH OR ENCRYPT? NOT CLEAR. ASK BRIAN
+            // save this password and salt for further use
+            encryptionKey = hashPassword(salt, userEnteredPassword);
 
+            // Encrypt some known data using the hashed password+salt as the encryption key
             String encryptedToken = encrypt(hashPassword(salt, userEnteredPassword),"hello");
 
-            //String base64Token = Base64.getEncoder().encodeToString(encryptedToken.getBytes());
+            assert encryptedToken != null;
+            String base64EncryptedToken = Base64.getEncoder().encodeToString(encryptedToken.getBytes());
             try {
-                addSaltAndToken(base64Salt, encryptedToken);
+                addSaltAndToken(base64Salt, base64EncryptedToken);
             }catch (Exception e){
                 System.out.println("Cannot write to file");
             }
@@ -125,15 +100,20 @@ public class PasswordManager {
                 String label = scanner.nextLine();
                 System.out.println("Enter a password to store:");
                 String password = scanner.nextLine();
-                addPassword(label, file, password);
+                // encrypt the password
+                String base64EncryptedPassword = encrypt(encryptionKey, password);
+                addPassword(label, file, base64EncryptedPassword);
 
 
             }else if (userChoice.equals("r")){
                 // read a password
                 System.out.println("Enter a label for the password:");
                 String label = scanner.nextLine();
-                // check that there exists a password associated with this label
-                // if it exits then print the password, else complain about bad label
+                byte[] encryptedPassword = getStoredPassword(label);
+                // decrypt the password
+                assert encryptedPassword != null;
+                String decryptedPassword = decrypt(encryptionKey, new String(encryptedPassword));
+                System.out.println(decryptedPassword);
             }else if (userChoice.equals("q")){
                 // Quit
                 System.out.println("Quitting");
@@ -147,12 +127,16 @@ public class PasswordManager {
 
 
     private static boolean verifyPassword(String userEnteredPassword, File file, String knownData) {
-        // Hash the salt + password and compare it to the hash stored in the secrets file
+        /*
+        Hash salt + user provided password
+        Use as key to encrypt known data
+        Compare encryption of known data to stored encryption in secrete.txt
+         */
         String[] saltAndToken = getSaltAndToken();
         assert saltAndToken != null;
         String decryptedToken = decrypt(hashPassword(Base64.getDecoder().decode(saltAndToken[0]), userEnteredPassword), saltAndToken[1]);
         if (decryptedToken == null)
-             return false;
+            return false;
         return decryptedToken.equals(knownData);
     }
 
@@ -175,8 +159,7 @@ public class PasswordManager {
             SecretKeySpec key = new SecretKeySpec(hash, "AES");
             cipher.init(Cipher.ENCRYPT_MODE, key);
             byte[] encryptedData = cipher.doFinal(knownData.getBytes());
-            String messageString = new String(Base64.getEncoder().encode(encryptedData));
-            return messageString;
+            return new String(Base64.getEncoder().encode(encryptedData));
 
         }
         catch (Exception e) {
@@ -195,8 +178,7 @@ public class PasswordManager {
             byte [] decrypted = cipher.doFinal(decoded);
             return new String(decrypted);
         }catch (Exception e){
-            //System.out.println("Issues decrypting password");
-            //e.printStackTrace();
+            System.out.println("Issues decrypting password");
         }
         return null;
 
@@ -204,10 +186,10 @@ public class PasswordManager {
 
     private static File getPasswordFile() {
         // Returns whether there exists a file with all of the  password manager information
-            //System.out.println("No password file detected. Creating a new password file.");
-            String currentPath = System.getProperty("user.dir");
-            String fileName = "secrets.txt";
-            return new File(currentPath, fileName);
+        //System.out.println("No password file detected. Creating a new password file.");
+        String currentPath = System.getProperty("user.dir");
+        String fileName = "secrets.txt";
+        return new File(currentPath, fileName);
     }
 
     private static void addSaltAndToken(String salt, String encryptedToken) {
@@ -231,8 +213,7 @@ public class PasswordManager {
             String[] saltAndToken = br.readLine().split(":");
             String base64Salt = saltAndToken[0];
             String base64Password = saltAndToken[1];
-            String[] vals = {base64Salt, base64Password};
-            return vals;
+            return new String[] {base64Salt, base64Password};
         }
         catch(IOException e) {
             System.out.println("Error reading file");
@@ -240,9 +221,29 @@ public class PasswordManager {
         return null;
     }
 
+
+    private static byte[] getStoredPassword(String label){
+        try {
+            FileReader fr = new FileReader("secrets.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while((line = br.readLine()) != null){
+                String[] labelAndPassword = line.split(":");
+                String lineLabel = labelAndPassword[0];
+                String base64Password = labelAndPassword[1];
+                if (lineLabel.equals(label)){
+                    return Base64.getDecoder().decode(base64Password);
+                }
+            }
+            System.out.println("This label/password pair does not exist on record");
+        }
+        catch(IOException e) {
+            System.out.println("Error retrieving password");
+        }
+        return null;
+    }
+
     private static void deleteLine(String label, String filePath, String newPassword) {
-
-
         Path path = Paths.get(filePath);
         boolean updated = false;
         String newLine = "";
@@ -287,7 +288,7 @@ public class PasswordManager {
                 key = keyValPair[0];
                 if (key.equals(label) && lineNumber > 1) {
 
-                    // The password will have to be encrypted. Here 'password' is assumed to be encrypted
+                    // The password will have to be encrypted. Here 'password' is assumed to be encrypted and in base64
 
                     deleteLine(label, file.getPath(), password);
                 }
